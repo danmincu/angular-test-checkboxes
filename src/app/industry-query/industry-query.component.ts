d80ad9d317f3d141959d9bc566e87663cfc260d4
danmincu <danmincu@gmail.com>
2023-04-28 04:30:50 -0400

    6 files modified

--- src/app/industry-query/industry-query.component.ts
+++ src/app/industry-query/industry-query.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject, of } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 interface Industry {
   name: string;
@@ -16,30 +18,54 @@ interface Activity {
 @Component({
   selector: 'app-industry-query',
   templateUrl: './industry-query.component.html',
-  styleUrls: ['./industry-query.component.css']
+  styleUrls: ['./industry-query.component.css'],
 })
 export class IndustryQueryComponent implements OnInit {
   queryString: string;
   industries: Industry[] = [];
 
-  constructor(private http: HttpClient) { }
+  industryActivitySubject: Subject<Industry> = new Subject<Industry>();
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    // Subscribe to the industryActivitySubject and debounce the API calls by 500ms
+    this.industryActivitySubject
+      .pipe(debounceTime(500))
+      .subscribe((industry: Industry) => {
+        this.getIndustryActivities(industry);
+      });
   }
+
+  ngOnInit(): void {}
+
+  // Add isLoading property
+  isLoading: boolean = false;
 
   getIndustries(): void {
-    this.http.get<Industry[]>(`https://api.intelpro.app/api/GoodsAndServices/v2/industies?question=${this.queryString}&withDescription=true`).subscribe((data: Industry[]) => {
-      this.industries = data;
-    });
+    this.isLoading = true; // Set isLoading to true before making the API call
+    this.http
+      .get<Industry[]>(
+        `https://api.intelpro.app/api/GoodsAndServices/v2/industies?question=${this.queryString}&withDescription=true`
+      )
+      .subscribe((data: Industry[]) => {
+        this.industries = data;
+        this.isLoading = false; // Set isLoading to false after getting the response
+      });
   }
 
+  // Update the getIndustryActivities method to be called by the industryActivitySubject
   getIndustryActivities(industry: Industry): void {
     if (!industry.checked) {
       industry.activities = [];
       return;
     }
-    this.http.get<Activity[]>(`https://api.intelpro.app/api/GoodsAndServices/v2/industryactivity?industryActivityTerm=${industry.name}&withDescription=true`).subscribe((data: Activity[]) => {
-      industry.activities = data;
-    });
+    this.isLoading = true; // Set isLoading to true before making the API call
+    this.http
+      .get<Activity[]>(
+        `https://api.intelpro.app/api/GoodsAndServices/v2/industryactivity?industryActivityTerm=${industry.name}%20industry&withDescription=true`
+      )
+      .subscribe((data: Activity[]) => {
+        industry.activities = data;
+        this.isLoading = false; // Set isLoading to false after getting the response
+      });
   }
 }
